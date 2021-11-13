9ce6a084528fc3e2f022e56a7ad6a6fcef1e854f
Magicst6 <48088266+Magicst6@users.noreply.github.com>
2021-11-13 10:37:15 +0100

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int firstNumber = 0, SecondNumber = 1, nextNumber, numberOfElements;
+            Console.Write("Geben Sie die Anzahl an Elementen ein : ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Bitte geben Sie eine Zahl größer als 2 an!");
+            }
+            else
+            {
+                //First print first and second number
+                Console.Write(firstNumber + " " + SecondNumber + " ");
+                //Starts the loop from 2 because 0 and 1 are already printed
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
